95462fcdd3de3e89088e30c119224f7a22ed75e6
Jhader2 <71098789+Jhader2@users.noreply.github.com>
2022-11-07 18:13:15 -0500

    Files Setup
    Went through youtube tutorial

---
+++ index.js
@@ -0,0 +1,58 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+const courses=[
+    {id:1, name: "course1"},
+    {id:2, name: "course2"},
+    {id:3, name: "course3"},
+]
+
+app.get('/',(req, res) => {
+    res.send("hello world");
+});
+
+app.post('/api/courses', (req,res)=>{
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body,schema);
+    console.log(result);
+
+    if(!req.body.name || req.body.name.length<3){
+        // 400 
+        res.status(400).send("Name too short");
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    course.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id',(req,res) => {
+    const course = courses.find(c=>c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send("course with that id not found");
+    res.send(course);
+});
+
+app.put('api/courses/:id', (req,res) => {
+    //Look up course
+    // If not existing return 404
+
+    //validate
+    //if invalid reutnr 400 - bad request
+
+    //uupdate course
+});
+
+// PORT
+const port = process.env.PORT||3000;
+
+app.listen(3000, () => console.log(`listening on ${port}`));
